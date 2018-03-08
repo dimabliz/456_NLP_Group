0d1f26faf4b7740907ba606ad011c54db5829965
Brandon G <Bgaetaniello@gmail.com>
2018-03-07 22:42:22 -0800

    added current trends

--- 456_Project.py
+++ 456_Project.py
@@ -1,4 +1,5 @@
-
+from nltk.twitter import Query, Streamer, Twitter, TweetViewer, TweetWriter, credsfromfile
+import tweepy
 
 def pre_process_words(tweet_words):
     for item in list(tweet_words):
@@ -20,21 +21,25 @@ def main():
     #export TWITTER="/path/to/twitter-files"
     #export TWITTER="/Users/dima/twitter-files"
 
-    from nltk.twitter import Query, Streamer, Twitter, TweetViewer, TweetWriter, credsfromfile
+
     oauth = credsfromfile()
-
+    auth = tweepy.OAuthHandler(oauth.get('app_key'), oauth.get('app_secret'))
+    auth.set_access_token(oauth.get('oauth_token'), oauth.get('oauth_token_secret'))
+    api = tweepy.API(auth)
+    seattleID = 2490383
+    trends = api.trends_place(seattleID)
+    trendNames = [trend['name'] for trend in trends[0]['trends']]
+    print(trendNames)
+    
     client = Query(**oauth)
-    tweets = client.search_tweets(keywords='arsenal', limit=10)
-
-    # tweet = next(tweets)
-    # from pprint import pprint
-    # pprint(tweet, depth=1)
+    print("\nCurrent Trend: %s\n" % trendNames[44])
+    tweets = client.search_tweets(keywords=trendNames[44], limit=10)
+    
 
     tweet_text = []
 
     for tweet in tweets:
         if tweet['lang'] == "en":
-            #print(tweet['text'])
             tweet_text.append(tweet['text'])
 
     #list of list of tweet words
