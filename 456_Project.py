99e50615eaf3c3d77469c029a648b0157fe9c103
Dmitriy Bliznyuk <dimabliz@uw.edu>
2018-03-11 23:25:18 -0700

    combined the sentiment rating for the tweets

--- 456_Project.py
+++ 456_Project.py
@@ -2,6 +2,7 @@ from nltk.twitter import Query, Streamer, Twitter, TweetViewer, TweetWriter, cre
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import tweepy
 
+
 def getTopTrends(name, oauth):
     auth = tweepy.OAuthHandler(oauth.get('app_key'), oauth.get('app_secret'))
     auth.set_access_token(oauth.get('oauth_token'), oauth.get('oauth_token_secret'))
@@ -13,6 +14,7 @@ def getTopTrends(name, oauth):
     trends = api.trends_place(ID)
     trendNames = [trend['name'] for trend in trends[0]['trends']]
     return trendNames
+
 
 def pre_process_words(tweet_words):
     for item in list(tweet_words):
@@ -31,6 +33,7 @@ def pre_process_words(tweet_words):
 
         if item.count == 0:
             tweet_words.remove(item)
+
 
 def get_final_tweet_sents(num_of_tweets, trend, oauth):
     processed_tweet_sents = []
@@ -53,32 +56,48 @@ def get_final_tweet_sents(num_of_tweets, trend, oauth):
 
     for i in tweet_words:
         processed_tweet_sents.append(" ".join(i))
-        
-    return processed_tweet_sents
 
+    return processed_tweet_sents
 
 
 def main():
     oauth = credsfromfile()
     # Retrieve the WOEID (Where on Earth ID) for Seattle
     city = "Seattle"
-    
+
     # Retrieve the top trends in Seattle
     trends = getTopTrends(city, oauth)
-    trend = trends[0]
+    trend = trends[2]
     tweets = get_final_tweet_sents(10, trend, oauth)
     analyser = SentimentIntensityAnalyzer()
-    
+
+    positivity = 0
+    negativity = 0
+    neutrality = 0
+
     print("\nCurrent Trend: %s\n" % trend)
     for tweet in tweets:
-         sentiment = analyser.polarity_scores(tweet)
-         print("{:-<40} {}".format(tweet, str(sentiment)))
-         if (sentiment['compound'] > 0 and sentiment['pos'] > 0):
-             print("This tweet was positive!\n")
-         elif (sentiment['compound'] < 0 and sentiment['neg'] > 0):
-             print("This tweet was negative!\n")
-         elif (sentiment['neu'] == 1):
-             print("This tweet was neutral!\n")
+        sentiment = analyser.polarity_scores(tweet)
+        print("{:-<40} {}".format(tweet, str(sentiment)))
+        if sentiment['compound'] > 0 and sentiment['pos'] > 0:
+            print("This tweet was positive!\n")
+            positivity += sentiment['pos']
+        elif sentiment['compound'] < 0 and sentiment['neg'] > 0:
+            print("This tweet was negative!\n")
+            negativity += sentiment['neg']
+        elif sentiment['neu'] == 1:
+            print("This tweet was neutral!\n")
+            neutrality += 1
+
+    print("positivity:" + str(positivity) + " negativity:" + str(negativity) + " neutrality:" + str(neutrality))
+
+    print("Topic: " + trends[1])
+    if positivity > negativity and positivity > negativity:
+        print("The topic is positive")
+    elif negativity > positivity and negativity > neutrality:
+        print("The topic is negative")
+    else:
+        print("The topic is neutral")
 
 if __name__ == "__main__":
     main()
