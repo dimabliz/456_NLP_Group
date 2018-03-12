118b89df23e2c6fa7546b4119ca04e1e470e3444
Dmitriy Bliznyuk <dimabliz@uw.edu>
2018-03-11 21:30:18 -0700

    Prepared the tweet text for sentiment analysis

--- 456_Project.py
+++ 456_Project.py
@@ -1,4 +1,6 @@
+import nltk
 from nltk.twitter import Query, Streamer, Twitter, TweetViewer, TweetWriter, credsfromfile
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import tweepy
 
 def pre_process_words(tweet_words):
@@ -13,35 +15,32 @@ def pre_process_words(tweet_words):
         if item[0] == "RT":
             item.remove("RT")
 
+        if item[0].startswith("@"):
+            item.remove(item[0])
+
         if item.count == 0:
             tweet_words.remove(item)
 
-def main():
-
-    #export TWITTER="/path/to/twitter-files"
-    #export TWITTER="/Users/dima/twitter-files"
-
-
+def get_final_tweet_sents(processed_tweet_sents, num_of_tweets):
     oauth = credsfromfile()
     auth = tweepy.OAuthHandler(oauth.get('app_key'), oauth.get('app_secret'))
     auth.set_access_token(oauth.get('oauth_token'), oauth.get('oauth_token_secret'))
     api = tweepy.API(auth)
-    
+
     # Retrieve the WOEID (Where on Earth ID) for Seattle
     seattleID = 0
     for trend in api.trends_available():
         if (trend['name'] == 'Seattle'):
-           seattleID = trend['woeid']
+            seattleID = trend['woeid']
 
     # Retrieve the current trends in Seattle
     trends = api.trends_place(seattleID)
     trendNames = [trend['name'] for trend in trends[0]['trends']]
     print(trendNames)
-    
+
     client = Query(**oauth)
     print("\nCurrent Trend: %s\n" % trendNames[0])
-    tweets = client.search_tweets(keywords=trendNames[0], limit=1000)
-    
+    tweets = client.search_tweets(keywords=trendNames[0], limit=num_of_tweets)
 
     tweet_text = []
 
@@ -49,7 +48,7 @@ def main():
         if tweet['lang'] == "en":
             tweet_text.append(tweet['text'])
 
-    #list of list of tweet words
+    # list of list of tweet words
     tweet_words = []
 
     for i in tweet_text:
@@ -57,9 +56,69 @@ def main():
 
     pre_process_words(tweet_words)
 
+    #processed_tweet_sents = []
+
     for i in tweet_words:
+        processed_tweet_sents.append(" ".join(i))
+
+    # print("processed sents")
+    # for i in processed_tweet_sents:
+    #     print(i)
+
+
+def main():
+
+    #export TWITTER="/path/to/twitter-files"
+
+    # oauth = credsfromfile()
+    # auth = tweepy.OAuthHandler(oauth.get('app_key'), oauth.get('app_secret'))
+    # auth.set_access_token(oauth.get('oauth_token'), oauth.get('oauth_token_secret'))
+    # api = tweepy.API(auth)
+    #
+    # # Retrieve the WOEID (Where on Earth ID) for Seattle
+    # seattleID = 0
+    # for trend in api.trends_available():
+    #     if (trend['name'] == 'Seattle'):
+    #        seattleID = trend['woeid']
+    #
+    # # Retrieve the current trends in Seattle
+    # trends = api.trends_place(seattleID)
+    # trendNames = [trend['name'] for trend in trends[0]['trends']]
+    # print(trendNames)
+    #
+    # client = Query(**oauth)
+    # print("\nCurrent Trend: %s\n" % trendNames[0])
+    # tweets = client.search_tweets(keywords=trendNames[0], limit=10)
+    #
+    #
+    # tweet_text = []
+    #
+    # for tweet in tweets:
+    #     if tweet['lang'] == "en":
+    #         tweet_text.append(tweet['text'])
+    #
+    # #list of list of tweet words
+    # tweet_words = []
+    #
+    # for i in tweet_text:
+    #     tweet_words.append(i.split())
+    #
+    # pre_process_words(tweet_words)
+    #
+    # processed_tweet_sents = []
+    #
+    # for i in tweet_words:
+    #     processed_tweet_sents.append(" ".join(i))
+    #
+    # print("processed sents")
+    # for i in processed_tweet_sents:
+    #     print(i)
+
+    processed_tweet_sents = []
+    get_final_tweet_sents(processed_tweet_sents, 10)
+    print("processed sents")
+    for i in processed_tweet_sents:
         print(i)
-        print()
 
 if __name__ == "__main__":
     main()
