a64767318fa07b2002ba8cc7d131ddcc7087e59f
Dmitriy Bliznyuk <dimabliz@uw.edu>
2018-03-11 21:31:40 -0700

    Got rid of unncessasary commented out code

--- 456_Project.py
+++ 456_Project.py
@@ -56,66 +56,17 @@ def get_final_tweet_sents(processed_tweet_sents, num_of_tweets):
 
     pre_process_words(tweet_words)
 
-    #processed_tweet_sents = []
-
     for i in tweet_words:
         processed_tweet_sents.append(" ".join(i))
-
-    # print("processed sents")
-    # for i in processed_tweet_sents:
-    #     print(i)
 
 
 def main():
 
     #export TWITTER="/path/to/twitter-files"
 
-    # oauth = credsfromfile()
-    # auth = tweepy.OAuthHandler(oauth.get('app_key'), oauth.get('app_secret'))
-    # auth.set_access_token(oauth.get('oauth_token'), oauth.get('oauth_token_secret'))
-    # api = tweepy.API(auth)
-    #
-    # # Retrieve the WOEID (Where on Earth ID) for Seattle
-    # seattleID = 0
-    # for trend in api.trends_available():
-    #     if (trend['name'] == 'Seattle'):
-    #        seattleID = trend['woeid']
-    #
-    # # Retrieve the current trends in Seattle
-    # trends = api.trends_place(seattleID)
-    # trendNames = [trend['name'] for trend in trends[0]['trends']]
-    # print(trendNames)
-    #
-    # client = Query(**oauth)
-    # print("\nCurrent Trend: %s\n" % trendNames[0])
-    # tweets = client.search_tweets(keywords=trendNames[0], limit=10)
-    #
-    #
-    # tweet_text = []
-    #
-    # for tweet in tweets:
-    #     if tweet['lang'] == "en":
-    #         tweet_text.append(tweet['text'])
-    #
-    # #list of list of tweet words
-    # tweet_words = []
-    #
-    # for i in tweet_text:
-    #     tweet_words.append(i.split())
-    #
-    # pre_process_words(tweet_words)
-    #
-    # processed_tweet_sents = []
-    #
-    # for i in tweet_words:
-    #     processed_tweet_sents.append(" ".join(i))
-    #
-    # print("processed sents")
-    # for i in processed_tweet_sents:
-    #     print(i)
-
     processed_tweet_sents = []
     get_final_tweet_sents(processed_tweet_sents, 10)
+
     print("processed sents")
     for i in processed_tweet_sents:
         print(i)
