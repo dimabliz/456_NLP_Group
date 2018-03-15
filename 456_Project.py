9dacd3982313840a6466730c0b8ac1211ea9af77
polive <parker.olive@comcast.net>
2018-03-14 18:45:42 -0700

    moved the number of tweets prompt to a method and added a check for a " " string in the search field

--- 456_Project.py
+++ 456_Project.py
@@ -4,7 +4,7 @@ import tweepy
 FAVE_RATIO = 0.001
 RETWEET_RATIO = 0.0001
 FOLLOWERS_RATIO = 0.00001
-    
+
 def getTopTrends(totalTrends, name, api):
     ID = 0
     for trend in api.trends_available():
@@ -56,11 +56,11 @@ def preprocess_tweet(raw_tweets):
 
     return processed_tweet_sents, retweet_counts, fave_counts, followers_counts
 
-#gets user input 
+#gets user input
 def get_user_input(options):
     invalid = 1
     while(invalid):
-        entry = input("Enter 1 for trends of the US: \n" 
+        entry = input("Enter 1 for trends of the US: \n"
                       + "Enter 2 for trends of Seattle: \n"
                       + "Enter 3 for trends of a city in the US: \n")
         selection = []
@@ -93,7 +93,7 @@ def get_user_input(options):
                 notInt = 0
             else:
                 print("Enter a total between 0 and 30.")
-                
+
     return totalTrends, selection
 
 def getOpinionTotals(tweets, retweet_counts, fave_counts, followers_count):
@@ -103,25 +103,25 @@ def getOpinionTotals(tweets, retweet_counts, fave_counts, followers_count):
     for i in range(len(tweets)):
         sentiment = analyser.polarity_scores(tweets[i])
         sentiments.append(sentiment)
-        
+
         if (sentiment['compound'] > 0 and sentiment['pos'] > 0):
-                  totals['Positive'] += (1 + 
-                  RETWEET_RATIO * retweet_counts[i] + 
-                  FAVE_RATIO * fave_counts[i] + 
-                  FOLLOWERS_RATIO * followers_count[i])
-                  
-        elif (sentiment['compound'] < 0 and sentiment['neg'] > 0):
-                  totals['Negative'] += (1 + 
-                  RETWEET_RATIO * retweet_counts[i] + 
-                  FAVE_RATIO * fave_counts[i] + 
-                  FOLLOWERS_RATIO * followers_count[i])
-                  
-        elif (sentiment['neu'] == 1):
-                  totals['Neutral'] += (1 + 
-                  RETWEET_RATIO * retweet_counts[i] + 
+                  totals['Positive'] += (1 +
+                  RETWEET_RATIO * retweet_counts[i] +
                   FAVE_RATIO * fave_counts[i] +
                   FOLLOWERS_RATIO * followers_count[i])
-                  
+
+        elif (sentiment['compound'] < 0 and sentiment['neg'] > 0):
+                  totals['Negative'] += (1 +
+                  RETWEET_RATIO * retweet_counts[i] +
+                  FAVE_RATIO * fave_counts[i] +
+                  FOLLOWERS_RATIO * followers_count[i])
+
+        elif (sentiment['neu'] == 1):
+                  totals['Neutral'] += (1 +
+                  RETWEET_RATIO * retweet_counts[i] +
+                  FAVE_RATIO * fave_counts[i] +
+                  FOLLOWERS_RATIO * followers_count[i])
+
     return sentiments, totals
 
 def getOpinionsOfTopic(topic, oauth, num_of_tweets):
@@ -139,7 +139,7 @@ def getOpinionsOfTopic(topic, oauth, num_of_tweets):
     neuPercent = totals['Neutral'] / adjustedTotal
     print("Opinions for the topic \"{}\":\nPositive: {:.0%}, Negative: {:.0%}, Neutral: {:.0%} out of {} tweets.\n"
                   .format(topic, posPercent, negPercent, neuPercent, num_of_tweets))
-    
+
     greatestTotal = float(max(totals.values()))
     opinion = ""
     for key in totals.keys():
@@ -149,7 +149,7 @@ def getOpinionsOfTopic(topic, oauth, num_of_tweets):
         print("The topic was mostly {}. Finding the most {} tweet.".format(opinion, opinion))
     else:
         print("The topic was mostly neutral. Unable to find the most neutral tweet.")
-    
+
     sent = {'pos' : 0, 'neg' : 0,'neu' : 0, 'compound' : 0}
     sentTweet = ""
     for i in range(len(tweets)):
@@ -161,12 +161,29 @@ def getOpinionsOfTopic(topic, oauth, num_of_tweets):
             if (sentiments[i]['compound'] <= sent['compound'] and sentiments[i]['neg'] > sent['neg']):
                 sent = sentiments[i]
                 sentTweet = raw_tweets[i]
-                
+
     if opinion != 'Neutral'.lower():
         print("Most {} tweet: {}".format(opinion, sentTweet['text']))
         print("URL: https://twitter.com/statuses/{}".format(sentTweet['id']))
     print("------------------------------------")
-        
+
+# Prompts the user to enter the number of tweets to analyze and re prompt if the
+# number is not in range or if the input is not a number
+def Number_of_Tweets():
+    notInt = 1
+    while (notInt):
+        num_of_tweets = input("Enter the number of tweets you want to analyze:\n")
+        try:
+            num_of_tweets = int(num_of_tweets)
+        except ValueError:
+            print("That's not an integer, try again")
+        else:
+            if num_of_tweets > 0 and num_of_tweets <= 500:
+                notInt = 0
+            else:
+                print("Enter a total between 0 and 500.")
+    return num_of_tweets
+
 def main():
     oauth = credsfromfile()
     print('Welcome to our Twitter Sentiment Analyzer!')
@@ -176,55 +193,33 @@ def main():
         entry = input("Enter 1 to search a topic: \n"
                       + "Enter 2 to analyze trends: \n")
         if entry == '1':
-            while keyword == "":
+            while keyword == "" or keyword == " ":
                 keyword = input("Enter a keyword or hashtag to search: ")
-            
-            notInt = 1
-            while (notInt):
-                num_of_tweets = input("Enter the number of tweets you want to analyze:\n")
-                try:
-                    num_of_tweets = int(num_of_tweets)
-                except ValueError:
-                    print("That's not an integer, try again")
-                else:
-                    if num_of_tweets > 0 and num_of_tweets <= 500:
-                        notInt = 0
-                    else:
-                        print("Enter a total between 0 and 500.")
+
+            num_of_tweets = Number_of_Tweets()
 
             getOpinionsOfTopic(keyword, oauth, num_of_tweets)
             invalid = 0
-    
+
         elif entry == '2':
             auth = tweepy.OAuthHandler(oauth.get('app_key'), oauth.get('app_secret'))
             auth.set_access_token(oauth.get('oauth_token'), oauth.get('oauth_token_secret'))
             api = tweepy.API(auth)
-    
+
             options = []
             for trend in api.trends_available():
                 if (trend['countryCode'] == 'US' and trend['name'] != 'United States'):
                     options.append(trend['name'])
-    
+
             totalTrends, location = get_user_input(options)
             trends = getTopTrends(totalTrends, location, api)
 
-            notInt = 1
-            while (notInt):
-                num_of_tweets = input("Enter the number of tweets you want to analyze:\n")
-                try:
-                    num_of_tweets = int(num_of_tweets)
-                except ValueError:
-                    print("That's not an integer, try again")
-                else:
-                    if num_of_tweets > 0 and num_of_tweets <= 500:
-                        notInt = 0
-                    else:
-                        print("Enter a total between 0 and 500.")
+            num_of_tweets = Number_of_Tweets()
 
             for trend in trends:
                 getOpinionsOfTopic(trend, oauth, num_of_tweets)
             invalid = 0
-        else: 
+        else:
             print("Invalid Selection. Try again.")
-    
+
 if __name__ == "__main__": main()
